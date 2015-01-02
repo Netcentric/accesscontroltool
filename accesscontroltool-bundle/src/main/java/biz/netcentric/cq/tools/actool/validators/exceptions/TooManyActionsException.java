4418028ff44f257cacec5e9acf5f97eaf33e13eb
Ugo Cei <ugo.cei@gmail.com>
2015-01-02 11:27:00 +0100

    Added license headers to all Java sources.

--- accesscontroltool-bundle/src/main/java/biz/netcentric/cq/tools/actool/validators/exceptions/TooManyActionsException.java
+++ accesscontroltool-bundle/src/main/java/biz/netcentric/cq/tools/actool/validators/exceptions/TooManyActionsException.java
@@ -1,3 +1,11 @@
+/*
+ * (C) Copyright 2015 Netcentric AG.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
 package biz.netcentric.cq.tools.actool.validators.exceptions;
 
 public class TooManyActionsException extends AcConfigBeanValidationException {
