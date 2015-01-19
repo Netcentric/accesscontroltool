971827b4666819b3adbeb8f85a1edc9e69527f25
Konrad Windszus <konrad.windszus@netcentric.biz>
2015-01-19 17:59:57 +0100

    add dummy installhook for the acservice

---
+++ accesscontroltool-bundle/src/main/java/biz/netcentric/cq/tools/actool/installhook/AcToolInstallHook.java
@@ -0,0 +1,42 @@
+package biz.netcentric.cq.tools.actool.installhook;
+
+import org.osgi.framework.ServiceReference;
+
+import biz.netcentric.cq.tools.actool.aceservice.AceService;
+
+import com.day.jcr.vault.packaging.InstallContext;
+import com.day.jcr.vault.packaging.PackageException;
+
+public class AcToolInstallHook extends OsgiAwareInstallHook {
+
+	@Override
+	public void execute(InstallContext context) throws PackageException {
+		switch (context.getPhase()) {
+		case PREPARE:
+			log("Installing ACLs through AcToolInstallHook...", context.getOptions());
+			ServiceReference aceServiceReference = getServiceReference("biz.netcentric.cq.tools.actool.aceservice.AceService");
+			if (aceServiceReference == null) {
+				throw new PackageException(
+						"Could not get AceService from OSGI service registry. Make sure the ACTool is installed!");
+			}
+			AceService aceService = (biz.netcentric.cq.tools.actool.aceservice.AceService) getBundleContext()
+					.getService(aceServiceReference);
+			if (aceService == null) {
+				throw new PackageException(
+						"Could not instanciate AceService. Make sure the ACTool is installed and check the log for errors");
+			}
+			try {
+				// TODO: refactor the AceService to be able to process arbitrary YAML files
+				aceService.execute();
+				log("Installed ACLs through AcToolInstallHook!", context.getOptions());
+			} finally {
+				getBundleContext().ungetService(aceServiceReference);
+			}
+			break;
+		default:
+			// nothing to do in all other phases
+			break;
+
+		}
+	}
+}
---
+++ accesscontroltool-bundle/src/main/java/biz/netcentric/cq/tools/actool/installhook/OsgiAwareInstallHook.java
@@ -0,0 +1,43 @@
+package biz.netcentric.cq.tools.actool.installhook;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.BundleReference;
+import org.osgi.framework.ServiceReference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.day.jcr.vault.fs.api.ProgressTrackerListener;
+import com.day.jcr.vault.fs.io.ImportOptions;
+import com.day.jcr.vault.packaging.InstallHook;
+
+public abstract class OsgiAwareInstallHook implements InstallHook {
+
+	private final BundleContext bundleContext;
+	private static final Logger LOG = LoggerFactory.getLogger(OsgiAwareInstallHook.class);
+
+	public OsgiAwareInstallHook() throws ClassCastException {
+		// https://gist.github.com/tux2323/1314120
+		bundleContext = BundleReference.class
+				.cast(OsgiAwareInstallHook.class.getClassLoader()).getBundle()
+				.getBundleContext();
+	}
+
+	public BundleContext getBundleContext() {
+		return bundleContext;
+	}
+
+	public ServiceReference getServiceReference(String clazz) {
+		ServiceReference serviceReference = bundleContext
+				.getServiceReference(clazz);
+		return serviceReference;
+	}
+
+	public void log (String message, ImportOptions options) {
+		ProgressTrackerListener listener = options.getListener();
+		if (listener != null) {
+			listener.onMessage(ProgressTrackerListener.Mode.TEXT, message, "");
+		} else {
+			LOG.info("message");
+		}
+	}
+}
