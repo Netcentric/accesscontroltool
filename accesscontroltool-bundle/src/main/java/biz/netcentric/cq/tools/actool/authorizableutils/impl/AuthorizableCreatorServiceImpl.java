2933c2ca3b930f313e35b92757161f578277c70f
Konrad Windszus <konrad.windszus@netcentric.biz>
2015-09-08 16:06:59 +0200

    do not fail with an NPE in case the user could not be created

--- accesscontroltool-bundle/src/main/java/biz/netcentric/cq/tools/actool/authorizableutils/impl/AuthorizableCreatorServiceImpl.java
+++ accesscontroltool-bundle/src/main/java/biz/netcentric/cq/tools/actool/authorizableutils/impl/AuthorizableCreatorServiceImpl.java
@@ -611,7 +611,7 @@ public class AuthorizableCreatorServiceImpl implements
             newUser.setProperty("profile/familyName", vf.createValue(familyName));
         }
 
-        if ((memberOf != null) && (memberOf.length > 0)) {
+        if ((newUser != null) && (memberOf != null) && (memberOf.length > 0)) {
 
             // add group to groups according to configuration
             Set<Authorizable> authorizables = validateAssignedGroups(
