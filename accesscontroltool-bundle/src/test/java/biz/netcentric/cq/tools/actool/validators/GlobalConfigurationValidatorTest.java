a0214dc6211caab869313d1640dc2e031a6994d3
georg.henzler <georg.henzler@netcentric.biz>
2016-06-24 23:33:20 +0200

    #40 copyright header

--- accesscontroltool-bundle/src/test/java/biz/netcentric/cq/tools/actool/validators/GlobalConfigurationValidatorTest.java
+++ accesscontroltool-bundle/src/test/java/biz/netcentric/cq/tools/actool/validators/GlobalConfigurationValidatorTest.java
@@ -1,3 +1,11 @@
+/*
+ * (C) Copyright 2015 Netcentric AG.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
 package biz.netcentric.cq.tools.actool.validators;
 
 import static org.junit.Assert.assertFalse;
