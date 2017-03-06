6be3e132ffc8bda4d54203dc20695279a2dc57ab
jochenkoschorke <jochen.koschorke@netcentric.biz>
2017-03-06 18:37:38 +0100

    #131-using super call to get set up beans from YamlConfigReader. Only add asserted exception after downcast. added overriden methods for creating TestBean types

--- accesscontroltool-bundle/src/test/java/biz/netcentric/cq/tools/actool/configreader/TestYamlConfigReader.java
+++ accesscontroltool-bundle/src/test/java/biz/netcentric/cq/tools/actool/configreader/TestYamlConfigReader.java
@@ -3,9 +3,12 @@ package biz.netcentric.cq.tools.actool.configreader;
 import java.util.Map;
 
 import biz.netcentric.cq.tools.actool.configmodel.AceBean;
+import biz.netcentric.cq.tools.actool.configmodel.AuthorizableConfigBean;
 
-/** Subclass of YamlConfigReader only used for unit tests. Creates a TestAceBean in order to set the assertedExceptionString set in test
- * yaml files for later evaluation in unit tests.
+/** Subclass of YamlConfigReader only used for unit tests. Overrides bean setup-methods from YamlConfigReader to set up TestAceBean and
+ * TestAuthorizableConfigBean in order to set the assertedExceptionString set in test yaml files for later evaluation in unit tests. Also
+ * overrides getNewAceBean() and getNewAuthorizableConfigBean() to return the correct testing type in order to make the downcast in
+ * overridden setup-methods possible.
  *
  * @author jochenkoschorke */
 public class TestYamlConfigReader extends YamlConfigReader {
@@ -13,12 +16,33 @@ public class TestYamlConfigReader extends YamlConfigReader {
     protected final String ASSERTED_EXCEPTION = "assertedException";
 
     @Override
-    protected AceBean setupAceBean(final String principal,
-            final Map<String, ?> currentAceDefinition) {
-        TestAceBean testAclBean = (TestAceBean) super.setupAceBean(principal, currentAceDefinition);
-        testAclBean.setAssertedExceptionString(getMapValueAsString(
+    protected void setupAceBean(final String principal,
+            final Map<String, ?> currentAceDefinition, final AceBean tmpAclBean) {
+        super.setupAceBean(principal, currentAceDefinition, tmpAclBean);
+        ((TestAceBean) tmpAclBean).setAssertedExceptionString(getMapValueAsString(
                 currentAceDefinition, ASSERTED_EXCEPTION));
-        return testAclBean;
+    }
+
+    @Override
+    protected void setupAuthorizableBean(
+            final AuthorizableConfigBean authorizableConfigBean,
+            final Map<String, String> currentPrincipalDataMap,
+            final String authorizableId,
+            boolean isGroupSection) {
+        super.setupAuthorizableBean(authorizableConfigBean, currentPrincipalDataMap, authorizableId, isGroupSection);
+        ((TestAuthorizableConfigBean) authorizableConfigBean).setAssertedExceptionString(getMapValueAsString(
+                currentPrincipalDataMap, ASSERTED_EXCEPTION));
+
+    }
+
+    @Override
+    protected AceBean getNewAceBean() {
+        return new TestAceBean();
+    }
+
+    @Override
+    protected AuthorizableConfigBean getNewAuthorizableConfigBean() {
+        return new TestAuthorizableConfigBean();
     }
 
 }
