efe90265fd4fcba7a2f81ed81046369387ac965b
jochenkoschorke <jochen.koschorke@netcentric.biz>
2017-03-06 12:21:41 +0100

    #131-removed code duplication in TestYamlConfigReader by using call to super.setupAceBean()

--- accesscontroltool-bundle/src/test/java/biz/netcentric/cq/tools/actool/configreader/TestYamlConfigReader.java
+++ accesscontroltool-bundle/src/test/java/biz/netcentric/cq/tools/actool/configreader/TestYamlConfigReader.java
@@ -15,30 +15,9 @@ public class TestYamlConfigReader extends YamlConfigReader {
     @Override
     protected AceBean setupAceBean(final String principal,
             final Map<String, ?> currentAceDefinition) {
-        AceBean testAclBean = new TestAceBean();
-        testAclBean.setPrincipal(principal);
-        testAclBean.setJcrPath(getMapValueAsString(currentAceDefinition,
-                ACE_CONFIG_PROPERTY_PATH));
-        testAclBean.setActionsStringFromConfig(getMapValueAsString(
-                currentAceDefinition, ACE_CONFIG_PROPERTY_ACTIONS));
-        testAclBean.setPrivilegesString(getMapValueAsString(
-                currentAceDefinition, ACE_CONFIG_PROPERTY_PRIVILEGES));
-        testAclBean.setPermission(getMapValueAsString(
-                currentAceDefinition, ACE_CONFIG_PROPERTY_PERMISSION));
-
-        testAclBean.setRestrictions(currentAceDefinition.get(ACE_CONFIG_PROPERTY_RESTRICTIONS),
-                (String) currentAceDefinition.get(ACE_CONFIG_PROPERTY_GLOB));
-        ((TestAceBean) testAclBean).setAssertedExceptionString(getMapValueAsString(
+        TestAceBean testAclBean = (TestAceBean) super.setupAceBean(principal, currentAceDefinition);
+        testAclBean.setAssertedExceptionString(getMapValueAsString(
                 currentAceDefinition, ASSERTED_EXCEPTION));
-        testAclBean.setActions(parseActionsString(getMapValueAsString(currentAceDefinition,
-                ACE_CONFIG_PROPERTY_ACTIONS)));
-
-        testAclBean.setKeepOrder(Boolean.valueOf(getMapValueAsString(currentAceDefinition,
-                ACE_CONFIG_PROPERTY_KEEP_ORDER)));
-
-        String initialContent = getMapValueAsString(currentAceDefinition,
-                ACE_CONFIG_INITIAL_CONTENT);
-        testAclBean.setInitialContent(initialContent);
         return testAclBean;
     }
 
