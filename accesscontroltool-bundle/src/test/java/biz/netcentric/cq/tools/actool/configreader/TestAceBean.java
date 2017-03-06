26b1c4eeb70676ea5eacf449831de174677ddd38
jochenkoschorke <jochen.koschorke@netcentric.biz>
2017-03-06 12:06:19 +0100

    #131-corrected description

--- accesscontroltool-bundle/src/test/java/biz/netcentric/cq/tools/actool/configreader/TestAceBean.java
+++ accesscontroltool-bundle/src/test/java/biz/netcentric/cq/tools/actool/configreader/TestAceBean.java
@@ -2,7 +2,7 @@ package biz.netcentric.cq.tools.actool.configreader;
 
 import biz.netcentric.cq.tools.actool.configmodel.AceBean;
 
-/** Subclass of AceBean only used for unit test. adding extra field for storing assertedExceptionStrings from test yaml files for later
+/** Subclass of AceBean only used for unit testing. Has extra field for storing assertedExceptionStrings from test yaml files for later
  * evaluation in unit tests
  *
  * @author jochenkoschorke */
@@ -11,14 +11,14 @@ public class TestAceBean extends AceBean {
     private String assertedExceptionString;
 
     /** sets a assertedExceptionString from a extra attribute in a test yaml files
-     * 
+     *
      * @param assertedException */
     public void setAssertedExceptionString(final String assertedException) {
         assertedExceptionString = assertedException;
     }
 
     /** returns the assertedExceptionString file stemming from the definition in yaml file this AceBean is based on
-     * 
+     *
      * @return */
     public String getAssertedExceptionString() {
         return assertedExceptionString;
