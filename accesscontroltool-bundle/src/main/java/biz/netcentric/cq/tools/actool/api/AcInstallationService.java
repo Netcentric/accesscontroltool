69bc683131d56eeaf143d7751bb2b7f0101e04fa
georg.henzler <georg.henzler@netcentric.biz>
2017-05-12 08:05:29 +0300

    license headers

--- accesscontroltool-bundle/src/main/java/biz/netcentric/cq/tools/actool/api/AcInstallationService.java
+++ accesscontroltool-bundle/src/main/java/biz/netcentric/cq/tools/actool/api/AcInstallationService.java
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright 2015 Netcentric AG.
+ * (C) Copyright 2017 Netcentric AG.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
