13f155606f57083bdff7f91baa2237d7cb7bf032
Konrad Windszus <konrad.windszus@netcentric.biz>
2019-10-15 13:20:33 +0200

    Improve robustness of yaml parser
    Throw more meaningful exceptions
    This closes #378

---
+++ accesscontroltool-bundle/src/main/java/biz/netcentric/cq/tools/actool/validators/exceptions/InvalidYamlException.java
@@ -0,0 +1,9 @@
+package biz.netcentric.cq.tools.actool.validators.exceptions;
+
+public class InvalidYamlException extends AcConfigBeanValidationException {
+
+    public InvalidYamlException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
