ab89cea06d048d30ae3c05e381c54d95c6e6d193
Konrad Windszus <konrad.windszus@netcentric.biz>
2020-11-19 08:39:37 +0100

    fix javadoc

--- accesscontroltool-bundle/src/main/java/biz/netcentric/cq/tools/actool/crypto/DecryptionService.java
+++ accesscontroltool-bundle/src/main/java/biz/netcentric/cq/tools/actool/crypto/DecryptionService.java
@@ -9,12 +9,12 @@
 package biz.netcentric.cq.tools.actool.crypto;
 
 /** Interface for decrypting encrypted text.
- * This allows to decouple from a concrete (AEM-specific) interface like {@link CryptoSupport} */
+ * This allows to decouple from a concrete (AEM-specific) interface like {@link com.adobe.granite.crypto.CryptoSupport} */
 public interface DecryptionService {
     
     /**
      * Decrypts the given parameter in case it is encrypted. Otherwise returns the given parameter unmodified.
-     * @param text
+     * @param text the potentially encrypted text
      * @return the decrypted text
      */
     public String decrypt(String text);
