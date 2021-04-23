a654e68f2bf0a4bfd98a5297aca6a8fcd1dc4bec
Oscar Farga <oscar.farga@netcentric.biz>
2021-04-23 10:23:48 +0200

    bugix-573 added missing import.

--- accesscontroltool-bundle/src/main/java/biz/netcentric/cq/tools/actool/configreader/YamlMacroProcessorImpl.java
+++ accesscontroltool-bundle/src/main/java/biz/netcentric/cq/tools/actool/configreader/YamlMacroProcessorImpl.java
@@ -18,6 +18,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
