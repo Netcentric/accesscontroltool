1a7712688a6ab103d7a04e86543e8275e86b9b7f
Konrad Windszus <konrad.windszus@netcentric.biz>
2024-07-10 17:07:19 +0200

    Adjust license headers

--- accesscontroltool-bundle/src/main/java/biz/netcentric/cq/tools/actool/authorizableinstaller/impl/AuthorizableInstallerServiceImpl.java
+++ accesscontroltool-bundle/src/main/java/biz/netcentric/cq/tools/actool/authorizableinstaller/impl/AuthorizableInstallerServiceImpl.java
@@ -1,12 +1,17 @@
-/*
- * (C) Copyright 2015 Netcentric AG.
- *
+package biz.netcentric.cq.tools.actool.authorizableinstaller.impl;
+
+/*-
+ * #%L
+ * Access Control Tool Bundle
+ * %%
+ * Copyright (C) 2015 - 2024 Cognizant Netcentric
+ * %%
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
+ * #L%
  */
-package biz.netcentric.cq.tools.actool.authorizableinstaller.impl;
 
 import static biz.netcentric.cq.tools.actool.helper.Constants.PRINCIPAL_EVERYONE;
 
