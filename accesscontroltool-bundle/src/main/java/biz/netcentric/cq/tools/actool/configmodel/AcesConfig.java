63567d0096c4f29e2243cdf2c8890b9e506d72f9
Konrad Windszus <konrad.windszus@netcentric.biz>
2024-07-10 17:07:19 +0200

    Adjust license headers

--- accesscontroltool-bundle/src/main/java/biz/netcentric/cq/tools/actool/configmodel/AcesConfig.java
+++ accesscontroltool-bundle/src/main/java/biz/netcentric/cq/tools/actool/configmodel/AcesConfig.java
@@ -1,12 +1,18 @@
-/*
- * (C) Copyright 2017 Netcentric AG.
- *
+
+package biz.netcentric.cq.tools.actool.configmodel;
+
+/*-
+ * #%L
+ * Access Control Tool Bundle
+ * %%
+ * Copyright (C) 2015 - 2024 Cognizant Netcentric
+ * %%
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
+ * #L%
  */
-package biz.netcentric.cq.tools.actool.configmodel;
 
 import java.util.LinkedHashSet;
 import java.util.Set;
--- accesscontroltool-bundle/src/main/java/biz/netcentric/cq/tools/actool/crypto/DecryptionService.java
+++ accesscontroltool-bundle/src/main/java/biz/netcentric/cq/tools/actool/crypto/DecryptionService.java
@@ -1,12 +1,17 @@
-/*
- * (C) Copyright 2019 Netcentric AG.
- *
+package biz.netcentric.cq.tools.actool.crypto;
+
+/*-
+ * #%L
+ * Access Control Tool Bundle
+ * %%
+ * Copyright (C) 2015 - 2024 Cognizant Netcentric
+ * %%
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
+ * #L%
  */
-package biz.netcentric.cq.tools.actool.crypto;
 
 /** Interface for decrypting encrypted text.
  * This allows to decouple from a concrete (AEM-specific) interface like {@link com.adobe.granite.crypto.CryptoSupport} */
