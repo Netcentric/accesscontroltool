0b18bb0e69cbe37610ec380a0e0c1c39063c015c
Konrad Windszus <konrad.windszus@netcentric.biz>
2024-08-14 09:27:49 +0200

    Add overall IMS update timeout
    Not set by default
    This closes #762

--- accesscontroltool-bundle/src/main/java/biz/netcentric/cq/tools/actool/ims/IMSUserManagement.java
+++ accesscontroltool-bundle/src/main/java/biz/netcentric/cq/tools/actool/ims/IMSUserManagement.java
@@ -118,6 +118,8 @@ public class IMSUserManagement implements ExternalGroupManagement {
         int connectTimeout() default 2000;
         @AttributeDefinition(name = "Socket Timeout", description = "The time waiting for data – after establishing the connection; maximum time of inactivity between two data packets. Given in milliseconds.")
         int socketTimeout() default 10000;
+        @AttributeDefinition(name = "Overall Timeout", description = "The maximum time updating groups in IMS should take. If expired the operation fails with an exception. Negative values mean no timeout. Given in milliseconds.")
+        long overallTimeout() default -1;
         @AttributeDefinition(name = "AEM Product Profiles", description = "The given product profile names are automatically added to each synchronized IMS group. The given product profile names must exist for an AEM product!")
         String[] productProfiles() default {};
         @AttributeDefinition(name = "Group Administrators", description = "The given users are automatically added to each synchronized IMS group as administrator. The given user ids must already exist!")
@@ -236,6 +238,7 @@ public class IMSUserManagement implements ExternalGroupManagement {
         if (config.isDifferentialUpdates()) {
             existingImsGroups = getGroups(token);
         }
+        long startEpoch = System.currentTimeMillis();
         List<ActionCommand> actionCommands = new LinkedList<>();
         List<String> updatedGroupNames = new LinkedList<>();
         for (AuthorizableConfigBean groupConfig : groupConfigs) {
@@ -287,6 +290,9 @@ public class IMSUserManagement implements ExternalGroupManagement {
             if (!response.warnings.isEmpty()) {
                 LOG.warn("Some warnings during updating groups with request {}", getRequestInfo(response.associatedRequest));
                 response.warnings.stream().forEach(w -> LOG.warn("Warning updating a group: {}", w));
+            }
+            if (config.overallTimeout() > 0 && System.currentTimeMillis() - startEpoch > config.overallTimeout()) {
+                throw new IOException("Timeout updating IMS groups, exceeded overall timeout of " + config.overallTimeout() + " ms");
             }
         }
         return updatedGroupNames.size();
